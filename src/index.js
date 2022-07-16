2d7e78cf2aad804afcfd3df2116718434ff314a6
JereUR <jereur.jdv@gmail.com>
2022-07-16 03:43:05 -0300

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
+
+fetchItems = () => {
+  fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((res) => (res.ok ? res.json() : Promise.reject()))
+    .then((json) => console.log(json));
+};
+
+document.addEventListener("DOMContentLoaded", fetchItems);
