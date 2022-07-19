b0c2ae0a0d56ee181fb97dbe63724af3ad38d749
JereUR <jereur.jdv@gmail.com>
2022-07-19 19:29:33 -0300

    Get movies and tv shows by genre with IMDB API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const API_URL = "https://api.themoviedb.org/3/";
 const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
 
-fetchMovieGenres = () => {
-  fetch(`${API_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`)
-    .then((res) => (res.ok ? res.json() : Promise.reject()))
-    .then((json) => console.log("Movie Genres", json));
-};
+let search = "drama, horror";
 
-fetchTvGenres = () => {
-  fetch(`${API_URL}genre/tv/list?api_key=${API_KEY}&language=en-US`)
+fetchIMDB = () => {
+  fetch(
+    `https://imdb-api.com/API/AdvancedSearch/k_w1v4f9bx?genre=${search}&languages=en`
+  )
     .then((res) => (res.ok ? res.json() : Promise.reject()))
-    .then((json) => console.log("TV Genres", json));
+    .then((json) => console.log(json));
 };
 
 document.addEventListener("DOMContentLoaded", (e) => {
-  fetchMovieGenres();
-  fetchTvGenres();
+  fetchIMDB();
 });
